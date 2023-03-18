35cade4362e8d68c34696728408ce0630dc1fbcf
satish1980 <kumar.sathish189@gmail.com>
2023-03-18 10:34:13 +0530

    student added

---
+++ src/test/java/sele/SeleniumFeb2023Automation/SecondFile.java
@@ -0,0 +1,15 @@
+package sele.SeleniumFeb2023Automation;
+
+public class SecondFile{
+	
+	public void add()
+	{
+		System.out.println("addition");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
