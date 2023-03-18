e81384a0e10a4812332636afd851d822241b8a84
satish1980 <kumar.sathish189@gmail.com>
2023-03-18 10:29:56 +0530

    add method

--- src/test/java/sele/SeleniumFeb2023Automation/firstfile.java
+++ src/test/java/sele/SeleniumFeb2023Automation/firstfile.java
@@ -1,6 +1,11 @@
 package sele.SeleniumFeb2023Automation;
 
 public class firstfile {
+	
+	public void add()
+	{
+		System.out.println("addition");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
