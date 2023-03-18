b2354cca1112950d85d1279e9db5a25308a0f86a
satish1980 <kumar.sathish189@gmail.com>
2023-03-18 10:48:16 +0530

    second

--- src/test/java/sele/SeleniumFeb2023Automation/Thridfile.java
+++ src/test/java/sele/SeleniumFeb2023Automation/Thridfile.java
@@ -6,6 +6,11 @@ public class Thridfile {
 	{
 		System.out.println("merge");
 	}
+	
+	public void mergeconflicts2()
+	{
+		System.out.println("merge");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
